105b4d6607772a0f63fc4b1f3dad330e9cbfa8d7
jhoel0521 <jhoel0521@gmail.com>
2025-11-11 19:05:56 -0400

    fix: corregir atributo fecha_inicio por inicio_real en vista jornadas index
    - Cambiar fecha_inicio por inicio_real en la vista de listado de jornadas
    - Soluciona error 'Call to a member function format() on null'

--- resources/views/supervisor/jornadas/index.blade.php
+++ resources/views/supervisor/jornadas/index.blade.php
@@ -38,7 +38,7 @@
                             <div class="text-sm text-gray-500">{{ $jornada->maquina->codigo }}</div>
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                            {{ $jornada->fecha_inicio->format('d/m/Y H:i') }}
+                            {{ $jornada->inicio_real->format('d/m/Y H:i') }}
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap">
                             <span class="px-3 py-1 inline-flex text-xs leading-5 font-semibold rounded-full
