5bc0f19e239d11adaf439a9c3f9e0664263e6039
Jhoel0521 <jhoel0521@gmail.com>
2025-11-11 15:15:12 -0400

    fix: corregir el orden de las importaciones en MaquinaController

--- app/Http/Controllers/Admin/MaquinaController.php
+++ app/Http/Controllers/Admin/MaquinaController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Maquina;
 use App\Models\Area;
+use App\Models\Maquina;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
